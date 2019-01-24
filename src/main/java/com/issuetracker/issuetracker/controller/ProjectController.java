13cfaa7826650450d7d140ed373edb0883bf86a5
Maja Mihajlovic <maja.mihajlovic9@gmail.com>
2019-01-24 10:39:48 +0100

    Small fix

--- src/main/java/com/issuetracker/issuetracker/controller/ProjectController.java
+++ src/main/java/com/issuetracker/issuetracker/controller/ProjectController.java
@@ -1,22 +1,22 @@
 package com.issuetracker.issuetracker.controller;
 
-import com.issuetracker.issuetracker.common.exception.BadRequestException;
-import com.issuetracker.issuetracker.model.Project;
-import com.issuetracker.issuetracker.model.User;
-import com.issuetracker.issuetracker.repository.ProjectRepository;
-import com.issuetracker.issuetracker.repository.UserRepository;
-import com.issuetracker.issuetracker.util.Validator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Scope;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+        import com.issuetracker.issuetracker.common.exception.BadRequestException;
+        import com.issuetracker.issuetracker.model.Project;
+        import com.issuetracker.issuetracker.model.User;
+        import com.issuetracker.issuetracker.repository.ProjectRepository;
+        import com.issuetracker.issuetracker.repository.UserRepository;
+        import com.issuetracker.issuetracker.util.Validator;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.context.annotation.Scope;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
-import java.util.List;
+        import javax.persistence.EntityManager;
+        import javax.persistence.PersistenceContext;
+        import javax.transaction.Transactional;
+        import java.util.List;
 
 @Controller
 @RequestMapping("api/project")
@@ -62,7 +62,7 @@ public class ProjectController extends GenericController<Project,Integer>{
     @Override
     public @ResponseBody
     List<Project> getAll() {
-       return cloner.deepClone(repository.getAllByFinnishedEquals((byte)0));
+        return cloner.deepClone(repository.getAllByFinnishedEquals((byte)0));
     }
     @Override
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -85,7 +85,7 @@ public class ProjectController extends GenericController<Project,Integer>{
             Project projectTemp = repository.findById(id).orElse(null);
             projectTemp.setName(project.getName());
             projectTemp.setDescription(project.getDescription());
-            projectTemp.setFinnished(project.getFinnished();
+            projectTemp.setFinnished(project.getFinnished());
             repo.saveAndFlush(projectTemp);
             return "Success";
         }throw new BadRequestException(badRequestUpdate);
